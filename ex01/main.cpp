22e30e4d852b10bc7a48c0c4442400b209cda556
usfachab <usfachb@gmail.com>
2023-10-06 20:46:48 +0100

    All good

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,30 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yachaab <yachaab@student.42.fr>            +#+  +:+       +#+        */
+/*   By: usfachab <usfachab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 00:04:35 by yachaab           #+#    #+#             */
-/*   Updated: 2023/10/01 16:28:34 by yachaab          ###   ########.fr       */
+/*   Updated: 2023/10/06 20:38:43 by usfachab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
-int main() {
-    ClapTrap user1( "Youssef" );
-    ScavTrap user2( "Hamza" );
+int main()
+{
 
-    std::cout << std::endl;
-    std::cout << "ClapTrap name: " << user1.getName() << std::endl;
-    std::cout << "ScavTrap name: " << user2.getName() << std::endl;
-    std::cout << std::endl;
-    user1.setDamagePoints( 10 );
-    user2.setDamagePoints( 10 );
-    std::cout << std::endl;
-    user1.attack( user2.getName() );
-    user2.takeDamage( user2.getDamagePoints() );
-    std::cout << user2.getHealthPoints() << std::endl;
-    std::cout << std::endl;
+    // COPY CONSTRUCTOR
+    // ScavTrap scav("Youssef");
+    // ScavTrap sc = scav;
+
+    // std::cout << scav.getHealthPoints() << std::endl;
+    // std::cout << sc.getHealthPoints() << std::endl;
+
+    // ClapTrap user1( "Youssef" );
+    // ScavTrap user2( "Hamza" );
+
+    // std::cout << std::endl;
+    // std::cout << "ClapTrap name: " << user1.getName() << std::endl;
+    // std::cout << "ScavTrap name: " << user2.getName() << std::endl;
+    // std::cout << std::endl;
+    // user1.setDamagePoints( 10 );
+    // user2.setDamagePoints( 10 );
+    // std::cout << std::endl;
+    // user1.attack( user2.getName() );
+    // user2.takeDamage( user2.getDamagePoints() );
+    // std::cout << user2.getHealthPoints() << std::endl;
+    // std::cout << std::endl;
 
     // std::cout << std::endl;
     // std::cout << "user2 name: " << user2.getName() << std::endl;
@@ -74,5 +83,5 @@ int main() {
     // user2.guardGate();
     // std::cout << std::endl;
 
-    return ( 0 );
+    return (0);
 }
